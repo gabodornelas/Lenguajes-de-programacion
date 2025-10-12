34a8738aad489d23a129cc555c7c7eae73a0fcd3
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2025-10-11 22:35:22 -0400

    Listo Pregunta1.b.i.go

--- Examen1/Pregunta1.b.i.go
+++ Examen1/Pregunta1.b.i.go
@@ -7,7 +7,7 @@ import (
     "strings"
 )
 
-// Funcion que rota k espacios la cadena de caracteres w y retorna la cadena rotada
+// funcion que rota k espacios la cadena de caracteres w y retorna la cadena rotada
 func rotar(w string, k int) string {
 	// convierte a runes para poder saber la longitud de la cadena
   	runes := []rune(w)
@@ -25,14 +25,14 @@ func rotar(w string, k int) string {
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	
-	// Leer cadena de caracteres
+	// leer cadena de caracteres
 	fmt.Println("Ingresa una cadena de caracteres: ")
 	// lee hasta conseguir salto de linea
 	cadena, _ := reader.ReadString('\n')
-	//limpia la cadena
+	// limpia la cadena
 	cadena = strings.TrimSpace(cadena)
 	
-	// Leer número entero
+	// leer número entero
 	fmt.Println("Ingresa un número entero no negativo: ")
 	numStr, _ := reader.ReadString('\n')
 	numStr = strings.TrimSpace(numStr)
