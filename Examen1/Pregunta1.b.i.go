9496ab2500c2e7cd117cf32e50270b875698fc6b
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2025-10-11 21:16:02 -0400

    Create Pregunta1.b.i.go

---
+++ Examen1/Pregunta1.b.i.go
@@ -0,0 +1,44 @@
+package main
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+)
+
+//Funcion que rota k espacios la cadena de caracteres w y retorna la cadena rotada
+func rotar(w string, k int) string {
+  //convierte a runes para poder saber la longitud de la cadena
+  runes := []rune(w)
+	longitud := len(runes)
+	if longitud > 0{
+    modulo := k % longitud
+    //se une la segunda parte de la cadena con la primera
+    rotado := append(runes[modulo:],runes[:modulo]...)
+    return string(rotado)
+	}else{
+	  return w
+	}
+}
+
+func main() {
+  reader := bufio.NewReader(os.Stdin)
+
+  // Leer cadena de caracteres
+  fmt.Println("Ingresa una cadena de caracteres: ")
+  cadena, _ := reader.ReadString('\n')
+  cadena = strings.TrimSpace(cadena)
+
+  // Leer número entero
+  fmt.Println("Ingresa un número entero no negativo: ")
+  numStr, _ := reader.ReadString('\n')
+  numStr = strings.TrimSpace(numStr)
+  numero, err := strconv.Atoi(numStr)
+  if err != nil || numero < 0 {
+      fmt.Println("Error: no ingresaste un número válido.")
+      return
+  }
+  
+  fmt.Println(rotar(cadena,numero))
+}
