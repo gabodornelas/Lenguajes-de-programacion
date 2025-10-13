85ae783a3c14890335a1c98ad062f8b8936788df
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2025-10-13 12:56:14 -0400

    Listo reto.go

--- Examen1/reto.go
+++ Examen1/reto.go
@@ -1,37 +1,19 @@
 package main
-
 import f "fmt"
-import m "math"
-
-func fa(n float64) float64{
-	if(n<2){
-		return n
-	}else{
-		return n*fa(n-1)
-	}
-}
-
-func fi(n float64) float64{
+import m "math" // importa Log2 y Floor
+func fi(n float64) float64{// funcion de fibonacci
 	if(n<2){
 		return n
 	}else{
 		return fi(n-1)+fi(n-2)
 	}
 }
-
 func main() {
-    var numero float64
-
-    f.Print("Número: ")
-    _, err := f.Scan(&numero)
+    var n float64
+    _, err := f.Scan(&n)
     if err != nil {
         f.Println("Error", err)
         return
-    }
-
-	fakm1 := fa(numero-2)
-	fak := fakm1*(numero-1)
-	fan := fak*numero*(numero+1)
-
-    f.Println("Wadefoc:", fi(m.Floor(m.Log2( ((fan*fan)/((numero+1)*fak*fakm1*12)) ))+1))
+    }// los numeros de narayana fueron simplificados
+    f.Println(fi(m.Floor(m.Log2(n*n*(n*n-1)/12))+1))
 }
