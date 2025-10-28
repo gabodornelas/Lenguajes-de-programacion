5733fcc6e462f68b8f1c5257ac4e868b2b4f9170
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2025-10-27 20:16:04 -0400

    Update Pregunta4.iterativa.c

--- Examen2/Pregunta4/Pregunta4.iterativa.c
+++ Examen2/Pregunta4/Pregunta4.iterativa.c
@@ -5,16 +5,16 @@
 long long funIter(long long n) {
     if(n <= 21) return n;
 
-    long long buffer[22]; // usamos un arreglo de 22 posiciones, (del 0 al 21) para acumular los valores
+    long long valores[22]; // usamos un arreglo de 22 posiciones, (del 0 al 21) para acumular los valores
 
-    for(long long i = 0; i <= 21; i++){ // se llena la primera instancia
-      buffer[i] = i;
+    for(long long i = 0; i <= 21; i++){ // se llena la primera instancia de los valores
+      valores[i] = i;
     }
 
     for(int i = 22; i <= n; i++){
-        buffer[i%22] = buffer[(i-7)%22] + buffer[(i-14)%22] + buffer[(i-21)%22];
+        valores[i % 22] = valores[(i-7) % 22] + valores[(i-14) % 22] + valores[(i-21) % 22];
     }
-    return buffer[n%22];
+    return valores[n % 22];
 }
 
 int main() {
